fccc984d4ddcd080ed53a4597d2663aeb0592db1
jparevalo <jparevalo@miuandes.cl>
2017-04-07 06:26:53 -0300

    Finished People Requirement

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,11 @@
 class Person < ApplicationRecord
   has_many :enrollements, through: :enrollments
+  validates :first_name, :last_name, :email, presence: true
+  validates :first_name, :last_name, length: { maximum: 30 }
+  validates :email, length: { maximum: 50 }
+  validates :email, uniqueness: true
+  validates :email, confirmation: { case_sensitive: false }
+  validates_format_of :email, :with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
 
   def to_s
     return first_name + ' ' + last_name
